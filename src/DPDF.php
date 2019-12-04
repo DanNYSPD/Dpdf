5a14c2318c88f1abb36359cd583dc0faeaaf1eae
Daniel <danie.jes1@gmail.com>
2019-12-03 22:16:36 -0600

    fixed  auto when ln is 2 and added new helping functions

--- src/DPDF.php
+++ src/DPDF.php
@@ -491,6 +491,8 @@ class DPDF extends FPDF{
             $arrayXYRigthPositions[]=[$this->GetX(),$this->GetY()];
             if($auto && $ln==0){
                 $this->SetXY($lastXMulticell,$lastYMulticell);
+            }else if($auto && 2==$ln){// 2 means below , so we keep x coordenate and move through y
+                $this->SetX($lastXMulticell);
             }
         }
         //ln to the right,1: to the beginning of the next line,2: below
@@ -1108,6 +1110,12 @@ class DPDF extends FPDF{
         $info = $this->_parsepngstream($f,$file);
         fclose($f);
         return $info;
+    }
+    public function GetPageBreakTrigger(){
+        return $this->PageBreakTrigger;
+    }
+    public function isPageBreakTriggerReached($nY=0):bool{
+        return $this->GetY()+$nY>$this->GetPageBreakTrigger();
     }
 
 }
